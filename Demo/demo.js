75844f5b07b6f777b6ef8f67edfe75916ed90019
Mike Shen <michael.m.shen@gmail.com>
2022-12-17 03:22:22 +0000

    added test files

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,8 +1,4 @@
 'use strict'
 
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-ctx.fillStyle = '#F0DB4F';
-ctx.strokeStyle = 'red';
-ctx.fillRect(50, 50, 150, 100);
-ctx.strokeRect(50, 50, 150, 100);
+let movie = { title: "The Matrix" };
+console.log(typeof(typeof(movie)));
